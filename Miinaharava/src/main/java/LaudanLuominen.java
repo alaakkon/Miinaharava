b101ba997ac630de1850d4f32f42e5ad1c1c039c
Laakkonen Anna <alaakkon@da3-cs-dk110-12.helsinki.fi>
2016-05-19 16:20:53 +0300

    Koodi

---
+++ Miinaharava/src/main/java/LaudanLuominen.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alaakkon
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import static oracle.jrockit.jfr.events.Bits.intValue;
+
+/**
+ *
+ * @author anna
+ */
+public class LaudanLuominen {
+
+    private int leveys;
+    private int korkeus;
+    private String vaikeustaso;
+    //  private String pelilauta;
+    private List<Integer> lauta2;
+    private int peli[][];
+    private Random arpoja;
+    private int[][] pel;
+
+    public LaudanLuominen(int korkeus, int leveys) {
+        this.leveys = leveys;
+        this.korkeus = korkeus;
+        this.vaikeustaso = vaikeustaso;
+        this.peli = new int[korkeus][leveys];
+        this.lauta2 = new ArrayList<Integer>();
+        this.pel = new int[korkeus + 2][leveys + 2];
+    }
+
+    public LaudanLuominen() {
+        this.leveys = leveys;
+        this.korkeus = leveys;
+        this.vaikeustaso = vaikeustaso;
+        this.peli = new int[korkeus][leveys];
+        this.lauta2 = new ArrayList<Integer>();
+    }
+
+    public int[][] luoLauta() {
+
+        arvoMiinatLaudalle();
+        merkitseVierustat();
+        return peli;
+    }
+
+    public int[][] arvoMiinatLaudalle() {
+
+        int koko = leveys * korkeus;
+        int rivi = 0;
+        int sarake = 0;
+        int luku = 0;
+        for (int i = 0; i < koko; i++) {
+            lauta2.add(i);
+
+        }
+        Collections.shuffle(lauta2);
+
+        double k = 0.15 * koko;
+        int miinoja = intValue(k);
+
+
+        for (int i = 0; i < miinoja; i++) {
+            luku = lauta2.get(i);
+            rivi = luku / leveys;
+            sarake = luku % leveys;
+
+            peli[rivi][sarake] = 9;
+        }
+        return peli;
+    }
+
+    public int[][] merkitseVierustat() {
+
+        for (int q = 0; q < 9; q++) {
+            for (int w = 0; w < 8; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q][w + 1] != 9) {//oikealla
+                        peli[q][w + 1] = peli[q][w + 1] + 1;
+                    }
+                }
+            }
+        }
+        for (int q = 1; q < 9; q++) {
+            for (int w = 0; w < 9; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q - 1][w] != 9) {//ylhäällä
+                        peli[q - 1][w] = peli[q - 1][w] + 1;
+                    }
+                }
+            }
+        }
+        for (int q = 0; q < 9; q++) {
+            for (int w = 0; w < 8; w++) {
+                if (peli[q][w + 1] == 9) {// miina
+                    if (peli[q][w] != 9) {//vasemmalla
+                        peli[q][w] = peli[q][w] + 1;
+                    }
+                }
+            }
+        }
+
+        for (int q = 0; q < 8; q++) {
+            for (int w = 0; w < 9; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q + 1][w] != 9) {//alhaalla
+                        peli[q + 1][w] = peli[q + 1][w] + 1;
+                    }
+                }
+            }
+        }
+        for (int q = 1; q < 9; q++) {
+            for (int w = 1; w < 9; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q - 1][w - 1] != 9) {//ylävasemmalla
+                        peli[q - 1][w - 1] = peli[q - 1][w - 1] + 1;
+                    }
+                }
+            }
+        }
+
+        for (int q = 1; q < 9; q++) {
+            for (int w = 0; w < 8; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q - 1][w + 1] != 9) {//yläoikealla
+                        peli[q - 1][w + 1] = peli[q - 1][w + 1] + 1;
+                    }
+                }
+            }
+        }
+        for (int q = 0; q < 8; q++) {
+            for (int w = 1; w < 9; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q + 1][w - 1] != 9) {//alavasen
+                        peli[q + 1][w - 1] = peli[q + 1][w - 1] + 1;
+                    }
+                }
+            }
+        }
+        for (int q = 0; q < 8; q++) {
+            for (int w = 0; w < 8; w++) {
+                if (peli[q][w] == 9) {// miina
+                    if (peli[q + 1][w + 1] != 9) {//alaoikea
+                        peli[q + 1][w + 1] = peli[q + 1][w + 1] + 1;
+                    }
+                }
+            }
+        }
+
+        return peli;
+
+    }
+
+    public int[][] haePelilauta() {
+        return peli;
+    }
+
+    public boolean onkoMiina(int x, int y) {
+        int onko = peli[x][y];
+        if (onko == 9) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public void tulosta() { //Tämä vielä tässä , siirretään kayttikseen
+        for (int i = 0; i < peli.length; i++) {
+            for (int j = 0; j < peli[0].length; j++) {
+                System.out.print(peli[i][j] + " ");
+            }
+            System.out.println();
+            System.out.println("");
+        }
+        // System.out.println(peli[0][0] + "," + peli[0][1] + "," + peli[0][2] + "," + peli[0][3] + "," + peli[0][4] + "," + peli[0][5]);
+    }
+}
+
